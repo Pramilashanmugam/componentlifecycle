07216fd363aa41bd50eb2dd2c26c7c9f10a98d01
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-09 21:19:49 +0000

    loader.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import css from './App.module.css';
+import NavBarChild from './components/NavBarChild';
+import NavBarForm from './components/NavBarForm';
+import NavBarSimple from './components/NavBarSimple';
 import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <Sidebar />
+      <Content /> 
+      <NavBarChild />
+      <NavBarForm />
+      <NavBarSimple />
     </div>
   );
 }
